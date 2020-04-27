df7276fe6ccd4fcd4eaa55759b77f82e541eceb5
Nirmal1992 <jainnirmal1992@gmail.com>
2020-04-27 17:08:17 +0530

    Initial commit

---
+++ src/NewGrudge.js
@@ -0,0 +1,35 @@
+import React, { useState, useMemo } from 'react';
+
+const NewGrudge = React.memo(({ onSubmit }) => {
+  const [person, setPerson] = useState('');
+  const [reason, setReason] = useState('');
+
+  const handleChange = event => {
+    event.preventDefault();
+    onSubmit({ person, reason });
+  };
+
+  console.log('New Grudges');
+
+  return (
+    <form className="NewGrudge" onSubmit={handleChange}>
+      <input
+        className="NewGrudge-input"
+        placeholder="Person"
+        type="text"
+        value={person}
+        onChange={event => setPerson(event.target.value)}
+      />
+      <input
+        className="NewGrudge-input"
+        placeholder="Reason"
+        type="text"
+        value={reason}
+        onChange={event => setReason(event.target.value)}
+      />
+      <input className="NewGrudge-submit button" type="submit" />
+    </form>
+  );
+});
+
+export default NewGrudge;
