cee5baab53a1d16e1a3031b1efcef9dc968dd89b
Nirmal1992 <jainnirmal1992@gmail.com>
2020-04-28 15:23:45 +0530

    optimized useContext

--- src/Application.js
+++ src/Application.js
@@ -1,60 +1,12 @@
-import React, { useCallback, useReducer } from 'react';
-
-import id from 'uuid/v4';
-
+import React from 'react';
 import Grudges from './Grudges';
 import NewGrudge from './NewGrudge';
 
-import initialState from './initialState';
-
-const ADD_GRUDGE = 'ADD_GRUDGE';
-const TOOGLE_GRUDGE = 'TOOGLE_GRUDGE';
-
-const reducer = (state, action) => {
-  if (action.type === ADD_GRUDGE) {
-    return [action.payload, ...state];
-  }
-  if (action.type === TOOGLE_GRUDGE) {
-    return state.map(grudge => {
-      if (grudge.id !== action.id) return grudge;
-      return { ...grudge, forgiven: !grudge.forgiven };
-    });
-  }
-  return state;
-};
-
 const Application = () => {
-  const [grudges, dispatch] = useReducer(reducer, initialState);
-
-  const addGrudge = useCallback(
-    ({ person, reason }) => {
-      dispatch({
-        type: ADD_GRUDGE,
-        payload: {
-          person,
-          reason,
-          id: id(),
-          forgiven: false
-        }
-      });
-    },
-    [dispatch]
-  );
-
-  const toggleForgiveness = useCallback(
-    id => {
-      dispatch({
-        type: TOOGLE_GRUDGE,
-        id
-      });
-    },
-    [dispatch]
-  );
-
   return (
     <div className="Application">
-      <NewGrudge onSubmit={addGrudge} />
-      <Grudges grudges={grudges} onForgive={toggleForgiveness} />
+      <NewGrudge />
+      <Grudges />
     </div>
   );
 };
---
+++ src/GrudgeContext.js
@@ -0,0 +1,69 @@
+import React, { createContext, useReducer, useCallback } from 'react';
+import id from 'uuid/v4';
+import initialState from './initialState';
+
+export const GrudgeContextState = createContext();
+export const GrudgeContextDispatch = createContext();
+
+const ADD_GRUDGE = 'ADD_GRUDGE';
+const TOOGLE_GRUDGE = 'TOOGLE_GRUDGE';
+
+export function GrudeProvider({ children }) {
+  const reducer = (state, action) => {
+    if (action.type === ADD_GRUDGE) {
+      return [action.payload, ...state];
+    }
+    if (action.type === TOOGLE_GRUDGE) {
+      return state.map(grudge => {
+        if (grudge.id !== action.id) return grudge;
+        return { ...grudge, forgiven: !grudge.forgiven };
+      });
+    }
+    return state;
+  };
+
+  const [grudges, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <GrudgeContextState.Provider value={grudges}>
+      <GrudgeContextDispatch.Provider value={dispatch}>
+        {children}
+      </GrudgeContextDispatch.Provider>
+    </GrudgeContextState.Provider>
+  );
+}
+
+export function useAddGrudge() {
+  const dispatch = React.useContext(GrudgeContextDispatch);
+  return useCallback(
+    ({ person, reason }) =>
+      dispatch({
+        type: ADD_GRUDGE,
+        payload: {
+          person,
+          reason,
+          id: id(),
+          forgiven: false
+        }
+      }),
+    [dispatch]
+  );
+}
+
+export function useToggleForgiveness() {
+  const dispatch = React.useContext(GrudgeContextDispatch);
+  return id => {
+    dispatch({
+      type: 'TOOGLE_GRUDGE',
+      id
+    });
+  };
+}
+
+export function useGrudges() {
+  const context = React.useContext(GrudgeContextState);
+  if (context === undefined) {
+    throw new Error('empty context');
+  }
+  return context;
+}
